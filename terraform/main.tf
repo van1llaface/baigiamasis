5f592aa4779f3d7e155d967cac352e9eb627dd90
van1llaface <arnoldaszeimis@gmail.com>
2025-04-14 20:05:37 +0300

    fixing main tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,11 @@ terraform {
   }
   required_version = ">= 1.2.0"
 }
+
 provider "aws" {
   region = "eu-north-1"
-
 }
+
 module "vpc" {
   source         = "terraform-aws-modules/vpc/aws"
   name           = "TODO-vpc"
@@ -18,36 +19,57 @@ module "vpc" {
   azs            = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
   public_subnets = ["10.0.101.0/24"]
 }
+
+resource "aws_security_group" "sg" {
+  name        = "my_security_group"
+  description = "Allow inbound traffic on port 3306 (MySQL) and port 22 (SSH)"
+  
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 module "ec2" {
   source             = "./app_server"
   sg_from_module     = [module.vpc.default_security_group_id]
   subnet_from_module = module.vpc.public_subnets[0]
 }
-provider "aws" {
-  region = "us-west-2"  # Update the region if needed
-}
 
 resource "aws_db_instance" "mydb" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "8.0"  # Choose the MySQL version
-  instance_class       = "db.t3.micro"
-  db_name              = "baigiasmasis_db"
-  username             = var.db_username
-  password             = var.db_password
+  allocated_storage  = 20
+  storage_type       = "gp2"
+  engine             = "mysql"
+  engine_version     = "8.0"
+  instance_class     = "db.t3.micro"
+  db_name            = "baigiasmasis_db"
+  username           = var.db_username
+  password           = var.db_password
   parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot  = true
-
-  # VPC and Security Group configurations for remote access
+  skip_final_snapshot = true
   vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
-# Output the DB connection string
 output "db_connect_string" {
   description = "MySQL database connection string"
-  value = "Server=${aws_db_instance.mydb.address}; Database=mydb; Uid=${var.db_username}; Pwd=${var.db_password}"
-  sensitive = true
+  value       = "Server=${aws_db_instance.mydb.address}; Database=mydb; Uid=${var.db_username}; Pwd=${var.db_password}"
+  sensitive   = true
 }
 
 variable "db_username" {
