035ac72bf606611d7d319e0d37d4952c23f6a41a
van1llaface <arnoldaszeimis@gmail.com>
2025-04-14 20:15:00 +0300

    no db for now

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,35 +52,35 @@ module "ec2" {
   subnet_from_module = module.vpc.public_subnets[0]
 }
 
-resource "aws_db_instance" "mydb" {
-  allocated_storage  = 20
-  storage_type       = "gp2"
-  engine             = "mysql"
-  engine_version     = "8.0"
-  instance_class     = "db.t3.micro"
-  db_name            = "baigiasmasis_db"
-  username           = var.db_username
-  password           = var.db_password
-  parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot = true
-  vpc_security_group_ids = [aws_security_group.sg.id]
-}
+# resource "aws_db_instance" "mydb" {
+#   allocated_storage  = 20
+#   storage_type       = "gp2"
+#   engine             = "mysql"
+#   engine_version     = "8.0"
+#   instance_class     = "db.t3.micro"
+#   db_name            = "baigiasmasis_db"
+#   username           = var.db_username
+#   password           = var.db_password
+#   parameter_group_name = "default.mysql8.0"
+#   skip_final_snapshot = true
+#   vpc_security_group_ids = [aws_security_group.sg.id]
+# }
 
-output "db_connect_string" {
-  description = "MySQL database connection string"
-  value       = "Server=${aws_db_instance.mydb.address}; Database=mydb; Uid=${var.db_username}; Pwd=${var.db_password}"
-  sensitive   = true
-}
+# output "db_connect_string" {
+#   description = "MySQL database connection string"
+#   value       = "Server=${aws_db_instance.mydb.address}; Database=mydb; Uid=${var.db_username}; Pwd=${var.db_password}"
+#   sensitive   = true
+# }
 
-variable "db_username" {
-  description = "Database Username"
-  default     = "adminas"
-  type        = string
-}
+# variable "db_username" {
+#   description = "Database Username"
+#   default     = "adminas"
+#   type        = string
+# }
 
-variable "db_password" {
-  description = "Database Password"
-  default     = "AdminAdmin1234."
-  type        = string
-  sensitive   = true
-}
+# variable "db_password" {
+#   description = "Database Password"
+#   default     = "AdminAdmin1234."
+#   type        = string
+#   sensitive   = true
+# }
