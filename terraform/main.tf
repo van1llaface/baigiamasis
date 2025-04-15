c887cbc3e0053898b318cfe8fa0725089f06bfee
van1llaface <arnoldaszeimis@gmail.com>
2025-04-15 20:33:36 +0300

    maybe jkow

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,7 +75,7 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "association" {
-  subnet_id      = aws_subnet.subnet.id
+  subnet_id = aws_subnet.main.id
   route_table_id = aws_route_table.route_table.id
 }
 
@@ -84,7 +84,7 @@ output "vpc_id" {
 }
 
 output "subnet_id" {
-  value = aws_subnet.subnet.id
+  value = aws_subnet.main.id
 }
 
 
