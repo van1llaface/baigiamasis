f0f3d8cb7e659ae99c2912db64c6056994364f62
van1llaface <arnoldaszeimis@gmail.com>
2025-04-15 20:31:32 +0300

    main fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,44 @@ resource "aws_instance" "example" {
   }
 }
 
+
+# Create an Internet Gateway for public access
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "MainInternetGateway"
+  }
+}
+
+# Create a route table and associate it with the subnet
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "MainRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "association" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.subnet.id
+}
+
+
 # resource "aws_db_instance" "mydb" {
 #   allocated_storage  = 20
 #   storage_type       = "gp2"
