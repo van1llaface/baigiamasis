af5d0b42a8e6fc55f2c381c35b7478de6ca6923a
van1llaface <arnoldaszeimis@gmail.com>
2025-04-15 19:42:54 +0300

    main tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,58 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0274f4b62b6ae3bd5" # Amazon Linux 2 AMI (example)
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
  
   user_data = file("user_data.sh")
  
   tags = {
     Name = "Baigiamasis-EC2-TODO"
   }
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow inbound HTTP, HTTPS, and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow custom port (e.g., 3000)"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
 
 # resource "aws_db_instance" "mydb" {
