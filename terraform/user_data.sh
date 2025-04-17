5e4fabacbe7d5311c3444256240169ff6128bb2d
van1llaface <arnoldaszeimis@gmail.com>
2025-04-17 18:26:06 +0300

    add compose d

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -6,9 +6,11 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
 
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 cd /home/ec2-user
 git clone https://github.com/van1llaface/baigiamasis.git
 cd baigiamasis
 
-sudo docker build -t baigiamasis-app .
-sudo docker run -d -p 3000:3000 baigiamasis-app
+docker compose up -d
