cba08743569d0d288792e46b0af6ed887bff3112
van1llaface <arnoldaszeimis@gmail.com>
2025-05-19 18:39:23 +0300

    fix user

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 sudo yum update -y
 sudo yum install docker -y
 sudo yum install git -y
-sudo service docker start
+sudo service docker start sleep 10
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
+
 
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
