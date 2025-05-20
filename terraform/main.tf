4a6aefc9f6d136b3e45869a054292ed264cb9b3f
van1llaface <arnoldaszeimis@gmail.com>
2025-05-20 21:22:45 +0300

    test1

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,31 +3,32 @@ provider "aws" {
 }
 
 # Create a security group to allow all traffic (insecure, for dev/testing)
-resource "aws_security_group" "allow_web" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id      = "vpc-03d1810d15c24c49b"
+# resource "aws_security_group" "allow_web" {
+#   name        = "allow_all"
+#   description = "Allow all inbound traffic"
+#   vpc_id      = "vpc-03d1810d15c24c49b"
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 # Launch EC2 instance
 resource "aws_instance" "example" {
   ami                    = "ami-02ec57994fa0fae21" # Replace with valid AMI
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_web.id]
+  # vpc_security_group_ids = [aws_security_group.allow_web.id]
+  vpc_security_group_ids = "sg-092e30ec949deb682"
 
   key_name      = "baigiamasis-pem"
 
