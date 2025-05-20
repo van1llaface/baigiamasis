ea6957426bcd72f09212ba96f5eb12f6562f08b1
van1llaface <arnoldaszeimis@gmail.com>
2025-05-20 18:07:55 +0300

    update user

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -9,7 +9,6 @@ sudo dnf install -y docker git
 
 # Start Docker service
 sudo systemctl start docker
-sleep 10
 
 # Add ec2-user to the docker group
 sudo usermod -aG docker ec2-user
@@ -18,14 +17,16 @@ sudo usermod -aG docker ec2-user
 sudo systemctl enable docker
 
 # Install Docker Compose (v2+ compatible)
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
-  -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Ensure ec2-user owns the home directory
+sudo chown -R ec2-user:ec2-user /home/ec2-user
 
 # Clone your repository
 cd /home/ec2-user
 git clone https://github.com/van1llaface/baigiamasis.git
 cd baigiamasis
 
-# Start your Docker app
-sudo -u ec2-user docker-compose up -d
+# Start your Docker app as ec2-user
+sudo -u ec2-user /usr/local/bin/docker-compose up -d
