b5fe766fda77dac846fc45ee0784c78f1050d357
van1llaface <arnoldaszeimis@gmail.com>
2025-05-20 21:20:36 +0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "allow_web" {
 
 # Launch EC2 instance
 resource "aws_instance" "example" {
-  ami                    = "ami-0274f4b62b6ae3bd5" # Replace with valid AMI
+  ami                    = "ami-02ec57994fa0fae21" # Replace with valid AMI
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_web.id]
 
@@ -38,37 +38,3 @@ resource "aws_instance" "example" {
   }
 }
 
-
-
-# resource "aws_db_instance" "mydb" {
-#   allocated_storage  = 20
-#   storage_type       = "gp2"
-#   engine             = "mysql"
-#   engine_version     = "8.0"
-#   instance_class     = "db.t3.micro"
-#   db_name            = "baigiasmasis_db"
-#   username           = var.db_username
-#   password           = var.db_password
-#   parameter_group_name = "default.mysql8.0"
-#   skip_final_snapshot = true
-#   vpc_security_group_ids = [aws_security_group.sg.id]
-# }
-
-# output "db_connect_string" {
-#   description = "MySQL database connection string"
-#   value       = "Server=${aws_db_instance.mydb.address}; Database=mydb; Uid=${var.db_username}; Pwd=${var.db_password}"
-#   sensitive   = true
-# }
-
-# variable "db_username" {
-#   description = "Database Username"
-#   default     = "adminas"
-#   type        = string
-# }
-
-# variable "db_password" {
-#   description = "Database Password"
-#   default     = "AdminAdmin1234."
-#   type        = string
-#   sensitive   = true
-# }
