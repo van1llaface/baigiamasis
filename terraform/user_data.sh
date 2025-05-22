a6d0ebc77fab1b96c8d8383d599f9021e477c80e
van1llaface <arnoldaszeimis@gmail.com>
2025-05-22 18:25:12 +0300

    lets see

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
+# exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
 
 # Update the system
 sudo yum update -y
@@ -22,4 +22,4 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 # Clone your repository
 git clone https://github.com/van1llaface/baigiamasis.git
-sudo docker-compose up -d
+sudo /usr/local/bin/docker-compose -f /home/ec2-user/baigiamasis/compose.yml up -d
