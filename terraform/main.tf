daafdbf639d98b9f3dc96714ea6bc1b9d27c52c1
van1llaface <arnoldaszeimis@gmail.com>
2025-05-29 18:32:50 +0300

    another ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ provider "aws" {
 
 # Launch EC2 instance
 resource "aws_instance" "example" {
-  ami                    = "ami-00f34bf9aeacdf007" #"ami-02ec57994fa0fae21"  Replace with valid AMI
+  ami                    = "ami-02ec57994fa0fae21"  #"ami-00f34bf9aeacdf007" Replace with valid AMI
   instance_type          = "t3.micro"
   # vpc_security_group_ids = [aws_security_group.allow_web.id]
   vpc_security_group_ids = ["sg-092e30ec949deb682"]
