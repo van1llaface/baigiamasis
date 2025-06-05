94f0dfa76d1bb8be71b58365859ccfff2b495676
van1llaface <arnoldaszeimis@gmail.com>
2025-06-05 19:51:34 +0300

    user fix

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -20,6 +20,7 @@ sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+sudo cd /home/ec2-user
 # Clone your repository
 sudo -u ec2-user git clone https://github.com/van1llaface/baigiamasis.git
 
